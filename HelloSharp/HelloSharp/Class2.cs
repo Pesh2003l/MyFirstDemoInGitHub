a20ed9fbd1b8c1b0721a1c8e83884574605a1e13
Pesh2003 <bulsnap@mail.bg>
2017-04-01 20:07:51 +0300

    Add A Program

---
+++ HelloSharp/HelloSharp/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSharp
+{
+    class Class2
+    {
+        public void Main()
+        {
+            Console.WriteLine("Hello there :)");
+        }
+    }
+}
